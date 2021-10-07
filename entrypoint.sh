d8f3461b92f553380c8216725491fd1e6ca0f536
Sparsh Kumar <34296252+bluehypergiant@users.noreply.github.com>
2021-10-07 13:00:49 +0000

    webdav to ftp

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ cat status | grep 'working\|maintenance'
 rclone version
 rclone config create 'CLOUDNAME' 'mega' 'user' $UserName 'pass' $PassWord
 rclone version
-rclone serve webdav CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes --user sparsh --pass sparsh
+rclone serve ftp CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes --user sparsh --pass sparsh
 
 
