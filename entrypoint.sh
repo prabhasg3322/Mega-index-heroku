cf818b92092ddcccf0323e0531a899cd5fb60994
Sparsh Kumar <34296252+bluehypergiant@users.noreply.github.com>
2021-10-07 14:04:26 +0000

    Added user and pass

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ cat status | grep 'working\|maintenance'
 rclone version
 rclone config create 'CLOUDNAME' 'mega' 'user' $UserName 'pass' $PassWord
 rclone version
-rclone serve webdav CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes
+rclone serve webdav CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes --user $UserName --pass $PassWord 
